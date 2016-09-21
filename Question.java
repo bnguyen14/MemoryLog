5bc1366e5486bb7e8763cfd852cb121ba5948ecd
Aroweeri <Dylanlsutton@gmail.com>
2016-09-21 13:17:25 -0400

    Add files via upload

---
+++ Question.java
@@ -0,0 +1,81 @@
+package quizletcopy;
+
+import java.util.ArrayList;
+public class Question {
+	
+	//Holds a list of several possible answers to a question.
+	private ArrayList<String> answers;
+	
+	//Holds a question.
+	private String question;
+	
+	public Question() {
+		this.question = null;
+		this.answers = null;
+	}//End constructor()
+	
+	public Question(ArrayList<String> answers, String question) {
+		this.answers = answers;
+		this.question = question;
+	}//End constructor(String, String)
+	
+	public Question(Question question) {
+		this(question.getAnswers(), question.getQuestion());
+	}//End constructor(Question)
+	
+	public Question(String record, String recordDelimiter) {
+		//Read the answers from the quiz record.
+		answers = new ArrayList<String>();
+		StringBuilder dynamicRecord = new StringBuilder(record);
+		StringBuilder nextAnswer = new StringBuilder();
+		while (dynamicRecord.toString().contains(recordDelimiter)) {
+			int nextDelimiterIndex = dynamicRecord.toString().indexOf(recordDelimiter);
+			for (int i = 0;i<nextDelimiterIndex;i++) {
+				nextAnswer.append(dynamicRecord.toString().charAt(i));
+			}
+			answers.add(nextAnswer.toString());
+			nextAnswer = new StringBuilder();
+			for (int i = 0;i<nextDelimiterIndex+recordDelimiter.length();i++) {
+				dynamicRecord.deleteCharAt(0);
+			}
+		}
+		//Read the question from the quiz record.
+		StringBuilder chopOffAnswers = new StringBuilder(record);
+		while (chopOffAnswers.toString().contains(recordDelimiter)) {
+			int nextDelimiterIndex = chopOffAnswers.indexOf(recordDelimiter);
+			for (int i = 0;i<nextDelimiterIndex+recordDelimiter.length();i++) {
+				chopOffAnswers.deleteCharAt(0);
+			}
+		}
+		question = chopOffAnswers.toString();
+	}
+	
+	//Returns a string that is written to a file to be read later.
+	public String toRecord(String recordDelimiter) {
+		StringBuilder sb = new StringBuilder();
+		//Write each answer that is in the answers ArrayList.
+		for (int i = 0;i<answers.size();i++) {
+			sb.append(answers.get(i) + recordDelimiter);
+		}
+			
+		//Write the question.
+		sb.append(question);
+		return sb.toString();
+	}//End toRecord()
+	
+	public void setAnswers(ArrayList<String> answers) {
+		this.answers = answers;
+	}//End setAnswers(Arraylist<String>)
+	
+	public ArrayList<String> getAnswers() {
+		return answers;
+	}//End getAnswers()
+	
+	public void setQuestion(String question) {
+		this.question = question;
+	}//End setQuestion(String)
+	
+	public String getQuestion() {
+		return question;
+	}//End getQuestion()
+}
---
+++ Quiz.java
@@ -0,0 +1,77 @@
+package quizletcopy;
+import java.util.ArrayList;
+
+/*
+ * Main purpose is to hold a number of Question objects.
+ */
+public class Quiz {
+	
+	private String path;
+	
+	//Holds the title of the quiz object.
+	private String title;
+	
+	//An identifier for what subject this quiz belongs to.
+	private int subjectIdentifier;
+	
+	//An array of questions that are all on the same subject.
+	private ArrayList<Question> questions;
+	
+	public Quiz() {
+		this(null,null,0);
+	}//End constructor()
+	
+	public Quiz(Quiz quiz) {
+			this.subjectIdentifier = quiz.getSubjectIdentifier();
+			this.path = quiz.getPath();
+			this.title = quiz.getTitle();
+			questions = new ArrayList<Question>();
+			for (int i = 0;i<quiz.getQuestions().size();i++) {
+				questions.add(new Question());
+				questions.get(i).setQuestion(quiz.getQuestions().get(i).getQuestion());
+				questions.get(i).setAnswers(new ArrayList<String>());
+				for (int j = 0;j<quiz.getQuestions().get(i).getAnswers().size();j++) {
+					questions.get(i).getAnswers().add(quiz.getQuestions().get(i).getAnswers().get(j));
+				}
+			}
+	}//End constructor(Quiz)
+	
+	public Quiz(String title, ArrayList<Question> questions, int subjectIdentifier) {
+		this.title = title;
+		this.questions = questions;
+		this.subjectIdentifier = subjectIdentifier;
+	}//End constructor(String, ArrayList<Question>, int)
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}//End setTitle(String)
+	
+	public String getTitle() {
+		return title;
+	}//End getTitle()
+	
+	public void setSubjectIdentifier(int subjectIdentifier) {
+		this.subjectIdentifier = subjectIdentifier;
+	}//End setSubjectIdentifier(int)
+	
+	public int getSubjectIdentifier() {
+		return subjectIdentifier;
+	}//End getSubjectIdentifier()
+	
+	public void setQuestions(ArrayList<Question> questions) {
+		this.questions = questions;
+	}//End setQuestions(ArrayList<Question>)
+	
+	public ArrayList<Question> getQuestions() {
+		return questions;
+	}//End getQuestions()
+	
+	public String getPath() {
+		return path;
+	}//End getPath()
+	
+	public void setPath(String path) {
+		this.path = path;
+	}//End setPath(String)
+	
+}//End Quiz

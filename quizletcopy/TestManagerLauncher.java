21ad56e9c2de57f98794421f1002d81cc4bfbcee
Aroweeri <Dylanlsutton@gmail.com>
2016-12-14 16:49:16 -0500

    Add files via upload

---
+++ quizletcopy/TestManagerLauncher.java
@@ -0,0 +1,8 @@
+package quizletcopy;
+
+public class TestManagerLauncher {
+	public static void main(String[] args) {
+		TestManager testManager = new TestManager();
+		testManager.runTestManager();
+	}
+}
